8bd37a005444ab9635517f10a139600a0843280d
Rownokzahan <84782117+Rownokzahan@users.noreply.github.com>
2023-03-29 02:55:55 +0600

    retrive an show data from localstorage

--- src/componets/Cart/Cart.jsx
+++ src/componets/Cart/Cart.jsx
@@ -3,19 +3,27 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrashCan, faArrowRight } from '@fortawesome/free-solid-svg-icons'
 
 const Cart = ({ cart }) => {
-    const totalPrice = cart.reduce((previous, current) => previous + current.price, 0);
-    const shippingCharge = cart.reduce((previous, current) => previous + current.shipping, 0);
+    let quantity = 0;
+    let totalPrice = 0;
+    let shippingCharge = 0;
+
+    for (const product of cart) {
+        totalPrice += product.price * product.quantity;
+        shippingCharge += product.shipping;
+        quantity += product.quantity;
+    }
+
     const tax = totalPrice * 7 / 100; // Tax is 7% of Total Cost
     const grandTotal = totalPrice + shippingCharge + tax;
     return (
         <div className='bg-plum p-6'>
             <h3 className='text-xl text-center font-semibold'>Order Summary</h3>
             <div className='text-gray my-12 flex flex-col gap-4'>
-                <p>Selected Items: { cart.length }</p>
-                <p>Total Price: ${ totalPrice }</p>
-                <p>Total Shipping Charge: ${ shippingCharge }</p>
-                <p>Tax: ${ tax.toFixed(2) }</p> {}
-                <h3 className='text-xl font-semibold'>Grand Total: ${ grandTotal.toFixed(2) }</h3>
+                <p>Selected Items: {quantity}</p>
+                <p>Total Price: ${totalPrice}</p>
+                <p>Total Shipping Charge: ${shippingCharge}</p>
+                <p>Tax: ${tax.toFixed(2)}</p> { }
+                <h3 className='text-xl font-semibold'>Grand Total: ${grandTotal.toFixed(2)}</h3>
             </div>
             <button className='bg-red w-full rounded text-white px-6 py-2 mb-4'>Clear Cart <FontAwesomeIcon className='ml-2' icon={faTrashCan} /></button>
             <button className='bg-orange w-full rounded text-white px-6 py-2'>Remove Order <FontAwesomeIcon className='ml-2' icon={faArrowRight} /></button>
--- src/componets/Shop.jsx
+++ src/componets/Shop.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Product from './Product';
 import Cart from './Cart/Cart';
+import { addToDb, getShoppingCart } from '../utilities/fakedb';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
@@ -12,9 +13,36 @@ const Shop = () => {
             .then(data => setProducts(data))
     }, []);
 
+    useEffect(() => {
+        const storedCart = getShoppingCart();
+        const savedCart = [];
+        for (const id in storedCart) {
+            const savedProduct = products.find(product => product.id === id);
+            if (savedProduct) {
+                const quantity = storedCart[id];
+                savedProduct.quantity = quantity;
+                savedCart.push(savedProduct);
+            }
+        }
+        setCart(savedCart);
+    }, [products]); // this products dependency makes sure products has been loaded
+
     const handleAddToCard = (product) => {
-        const newCart = [...cart, product];
+        let newCart = [];
+
+        const exits = cart.find(pd => pd.id === product.id);
+        if (!exits) {
+            product.quantity = 1;
+            newCart = [...cart, product];
+        }
+        else {
+            product.quantity += 1;
+            const remaining = cart.filter(pd => pd.id !== product.id);
+            newCart = [...remaining, product];
+        }
+
         setCart(newCart);
+        addToDb(product.id);
     }
 
     return (
